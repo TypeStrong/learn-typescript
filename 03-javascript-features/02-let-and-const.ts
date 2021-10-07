e08aa1d491f3d2a915bba1ce495d0dd80e1360dc
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-10-07 10:11:37 +0330

    Remove the unclarity about `const` variables (#5)

--- 03-javascript-features/02-let-and-const.ts
+++ 03-javascript-features/02-let-and-const.ts
@@ -1,13 +1,13 @@
 // Both `let` and `const` are ES6 additions which use block scoping, instead of the traditional function scope used by `var`.
 
-// Using `const` helps greatly with readability and understandability. Any new programmer immediately knows this value is immutible - it won't be changed further down in the execution.
+// Using `const` helps greatly with readability and understandability. Any new programmer immediately knows this value is a constant - it won't be changed further down in the execution.
 const value = 'test'
 
 // value = 'new value' //=> Error: Left-hand side of assignment expression cannot be a constant.
 
 const array = [1, 2, 3]
 
-// I recommend only using `let` when the value is not immutable - such as in a loop.
+// I recommend only using `let` when the value will be changed - such as in a loop.
 for (let i = 0; i < array.length; i++) {
   console.log(array[i])
 }
